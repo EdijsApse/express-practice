67697df749e6265d42099f05e2dce1c2e035b246
Edijs <edijsapse@gmail.com>
2022-05-22 10:25:30 +0300

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('landing');
+});
+
+app.listen(3000);
