881d2a7927451ebf4244da0835d7b9bd444ff510
Edijs <edijsapse@gmail.com>
2022-05-23 12:20:51 +0300

    Code spliting & image file picker

---
+++ public/scripts/image-picker.js
@@ -0,0 +1,47 @@
+const fileInputPlaceholder = document.querySelector('.file-select-container');
+
+if (fileInputPlaceholder) {
+    prepareFilePicker();
+}
+
+function prepareFilePicker() {
+    const input = document.querySelector('.file-select-input');
+
+    fileInputPlaceholder.addEventListener('click', () => {    
+        input.click();
+    });
+
+    input.addEventListener('change', () => {
+        const files = input.files;
+        [].forEach.call(files, showSelectedFiles)
+    });
+}
+
+
+function showSelectedFiles(file) {
+    const fileReader = new FileReader();
+    const image = new Image();
+    const previewList = document.querySelector('.selected-files-preview');
+    const imagePreviewCol = document.createElement('div');
+    const imagePreview = document.createElement('div');
+    
+    imagePreviewCol.classList.add('file-preview-col');
+    imagePreview.classList.add('file-preview-container');
+    imagePreviewCol.appendChild(imagePreview);
+    previewList.appendChild(imagePreviewCol);
+    
+    fileReader.readAsDataURL(file);
+
+    fileReader.addEventListener('load', () => {
+        image.src = fileReader.result;
+        imagePreview.appendChild(image)
+        setTimeout(() => {
+            imagePreview.classList.remove('spinner-grow');
+            image.classList.add('anim-show')
+        }, 750)
+    })
+
+    fileReader.addEventListener('loadstart', () => {
+        imagePreview.classList.add('spinner-grow');
+    })
+}
